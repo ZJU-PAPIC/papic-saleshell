5db2797667abace4757ea9eb2aa63a7ead1b81c6
wkk5194 <kysoowjk@163.com>
2019-09-15 10:18:07 +0800

    :children_crossing: add 404 page

--- saleshell/src/pages/NotFound.js
+++ saleshell/src/pages/NotFound.js
@@ -1,3 +1,17 @@
-import React from 'react'
-const Notfound = () => <h1>Not found</h1>
-export default Notfound
+import React from "react";
+import { Result, Button } from "antd";
+import { Link } from "react-router-dom";
+
+const Notfound = () => (
+  <Result
+    status="404"
+    title="404"
+    subTitle="Sorry, the page you visited does not exist."
+    extra={
+      <Link to="/">
+        <Button type="primary">Back Home</Button>
+      </Link>
+    }
+  />
+);
+export default Notfound;
